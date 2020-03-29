e710f90a6f7bce14fc8a396bd0e6f34c309f6a31
doulank <t_tlovedou@163.com>
2020-03-29 20:47:00 +0800

    first commit

---
+++ RCT6/arch/risc-v/spike/gcc/riscv_port_c.c
@@ -0,0 +1,9 @@
+#include "tos.h"
+
+__PORT__ void port_cpu_init() {
+    // DO NOTHING
+}
+
+__PORT__ void port_systick_priority_set(uint32_t priority) {
+    // DO NOTHING
+}
---
+++ RCT6/board/ALIENTEK-MiniSTM32F103RCT6/KEIL/hello_world/ALIENTEK-MiniSTM32F103RCT6/port_s.d
@@ -0,0 +1,1 @@
+alientek-ministm32f103rct6\port_s.o: ..\..\..\..\arch\arm\arm-v7m\cortex-m3\armcc\port_s.S
